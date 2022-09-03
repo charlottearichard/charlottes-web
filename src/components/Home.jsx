a6f0d49b0d15030f625905eb82e91a9d9268f793
Charlotte <charlottealiciarichard@gmail.com>
2022-09-03 15:00:12 -0400

    Home And Welcome

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+function Home() {
+  return (
+    <div name='home' className='w-full h-screen bg-[#f5f7f8]'>
+
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <h2 className='font-[Inconsolata] text-3xl  text-[#8da4b2]'>hello world, my name is </h2>
+        <h2 className='font-[Inconsolata] text-7xl  text-[#6a8596]'> Charlotte Ri'Chard </h2>
+        <p className='font-[Inconsolata] text-5xl  text-[#b9c9d4]'> I am a web developer </p>
+    <div>
+        <button className="py-3 font-[Inconsolata] text-2xl text-[#708897] border-4 border-[#708897] px-6 my-2 flex items-center hover:bg-[#708897] hover:text-[#e4ebef]" > LEARN MORE <HiArrowNarrowRight /> </button>
+    </div>
+    
+    </div>
+
+   
+
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
     return(
-        <div className='fixed w-full h-[80px] flex justify-between items center px-4 bg-[#f5f7f8] text-[#2d383f]'> 
+        <div className='fixed w-full h-[80px] flex justify-between items center px-4 bg-[#f5f7f8] text-[#59707d]'> 
         <div>
          <h1 className='text-3xl'>   CHARLOTTE'S WEB </h1>
         </div>
@@ -27,18 +27,14 @@ const Navbar = () => {
         </div>
 
         {/* Mobile */}
-        <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#f5f7f8] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-3xl text-[#688393] '> Home </li>
-                <li className='py-6 text-3xl text-[#688393]'> About </li>
-                <li className='py-6 text-3xl text-[#688393]'> Portfolio </li>
-                <li className='py-6 text-3xl text-[#688393]'> Contact </li>
-                <li className='py-6 text-3xl text-[#688393]'> Resume </li>
+        <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#f0f3f5] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-3xl text-[#59707d] '> Home </li>
+                <li className='py-6 text-3xl text-[#59707d]'> About </li>
+                <li className='py-6 text-3xl text-[#59707d]'> Portfolio </li>
+                <li className='py-6 text-3xl text-[#59707d]'> Contact </li>
+                <li className='py-6 text-3xl text-[#59707d]'> Resume </li>
 
             </ul>
-
-
-        {/* Socials */}
-        <div className='hidden'></div>
 
 
         </div>
